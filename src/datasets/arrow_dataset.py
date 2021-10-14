a308c4c4946552b50cc5efe9b5ac5b2301a497f5
matt <rocketknight1@gmail.com>
2021-10-14 16:28:24 +0100

    More special-casing around labels

--- src/datasets/arrow_dataset.py
+++ src/datasets/arrow_dataset.py
@@ -396,6 +396,14 @@ class TensorflowDatasetMixIn:
 
             tf_dataset = tf_dataset.map(add_dummy_labels)
 
+        def rename_label_col(input_batch):
+            if 'label' in input_batch:
+                input_batch['labels'] = input_batch['label']
+                del input_batch['label']
+            return input_batch
+
+        tf_dataset = tf_dataset.map(rename_label_col)
+
         if prefetch:
             tf_dataset = tf_dataset.prefetch(tf.data.AUTOTUNE)
         return tf_dataset
